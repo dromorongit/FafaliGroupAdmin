2a442148626ba8c384cffc448869c9eacc9ec899
Narh Henry Padi Dromor <hpdnarh@gmail.com>
2026-01-07 14:54:55 +0000

    New Changes

--- admin/src/models/AdminUser.js
+++ admin/src/models/AdminUser.js
@@ -4,7 +4,7 @@
  */
 
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const adminUserSchema = new mongoose.Schema({
   name: {
--- admin/src/utils/seedAdmin.js
+++ admin/src/utils/seedAdmin.js
@@ -6,7 +6,7 @@
 
 require('dotenv').config();
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const AdminUser = require('../models/AdminUser');
 
